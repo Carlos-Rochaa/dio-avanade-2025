f990c92dfd4a3c409c35553321d7c308d98a76e0
Carlos Rocha <carlosrocha1896@gmail.com>
2025-03-20 10:55:40 -0300

    Criação base do projeto

---
+++ src/main/java/me/dio/model/Card.java
@@ -0,0 +1,25 @@
+package me.dio.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "tb_card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private BigDecimal number;
+    private BigDecimal limit;
+}

674dd0a0152085f0d787c29c4f47fdbfc3df6473
Carlos Rocha <carlosrocha1896@gmail.com>
2025-03-20 10:04:14 -0300

    Initial commit

---
+++ src/main/java/me/dio/DioAvanade2025Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioAvanade2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioAvanade2025Application.class, args);
+	}
+
+}
